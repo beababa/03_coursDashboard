6713979852d9c8ce0905cbd911a82e009e554b87
tachasa <beatrice.maranget@cnrs.fr>
2021-01-28 20:05:07 +0100

    rpgfinale

--- dataS/rqt.sql
+++ dataS/rqt.sql
@@ -1,6 +1,7 @@
 
-CREATE TABLE lien AS (
-SELECT canton.fid, code_cultu, sum(surf_parc)  AS surface
+
+CREATE TABLE data2019 AS (
+SELECT canton.fid, code_cultu, sum(surf_parc)  AS surface, canton.geom
 FROM parcelle_graphique, canton
 	WHERE ST_INTERSECTS(st_centroid(parcelle_graphique.geom), canton.geom)
 	GROUP BY code_cultu, canton.fid

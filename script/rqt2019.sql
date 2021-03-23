68a5d09b3f0d9875673572104ed856dbe5cec712
maranb <b.maranget@arvalis.fr>
2021-03-23 13:42:45 +0100

    maj

--- script/rqt2019.sql
+++
@@ -1,8 +0,0 @@
-
-
-CREATE TABLE data2019 AS (
-SELECT canton.fid, canton.id_geofla,code_cultu, sum(surf_parc)  AS surface
-FROM parcelle_graphique, canton
-	WHERE ST_INTERSECTS(st_centroid(parcelle_graphique.geom), canton.geom)
-	GROUP BY code_cultu, canton.fid
-	);
